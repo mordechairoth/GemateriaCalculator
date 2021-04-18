7bb7a38ca62a64a68a184171b914ba1cf776958e
motty roth <mroth@techonsoft.com>
2021-04-18 18:49:30 -0400

    created method to calculate all consecutive numbers in IEnumerabel that add up to sum, test passed

---
+++ GemateriaAssignment/Logic/FindGemateriaInTextFile.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GemateriaAssignment.FindMatches
+{
+    public class FindGemateriaMathces
+    {
+        public Dictionary<int, int> GetAllSums(IEnumerable<int> numbers, int sum)
+        {
+            Dictionary<int, int> indexCount = new Dictionary<int, int>();
+
+            List<int> cachedNumbers = new List<int>();
+            int startIndex = 0;
+            int finishIndex = -1;
+            
+
+            foreach (var number in numbers)
+            {
+                cachedNumbers.Add(number);
+                finishIndex++;
+               
+                if (cachedNumbers.Sum() > sum)
+                {
+                    if (cachedNumbers.Count == 1)
+                    {
+                        startIndex = finishIndex + 1;
+                    }
+                    else if (cachedNumbers.Count() > 1)
+                    {
+                        int cachedNumbersCount = cachedNumbers.Count();
+                        for (var i = 0; i < cachedNumbersCount; i++)
+                        {
+                            cachedNumbers.RemoveAt(0);
+                            startIndex++;
+                            
+                            if (cachedNumbers.Sum() > sum)
+                            {
+                                if (cachedNumbers.Count == 1)
+                                {
+                                    startIndex = finishIndex + 1;
+                                    cachedNumbers.Clear();
+                                    break;
+                                }
+                                else if (cachedNumbers.Count > 1)
+                                {
+                                    continue;
+                                }
+                            }
+                            
+                            if (cachedNumbers.Sum() == sum)
+                            {
+                                indexCount.Add(startIndex, cachedNumbers.Count());
+                                startIndex++;
+                                if (cachedNumbers.Count == 1)
+                                {
+                                    cachedNumbers.Clear();
+                                    break;
+                                }
+                                else if (cachedNumbers.Count > 1)
+                                {
+                                    cachedNumbers.RemoveAt(0);
+                                    break;
+                                }
+                            }
+
+                            if (cachedNumbers.Sum() < sum)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                   
+                }
+                else if (cachedNumbers.Sum() == sum)
+                {
+                    indexCount.Add(startIndex, cachedNumbers.Count());
+                    if (cachedNumbers.Count == 1)
+                    {
+                        cachedNumbers.Clear();
+                        startIndex = finishIndex + 1;
+                    }
+                    else if (cachedNumbers.Count > 1)
+                    {
+                        cachedNumbers.RemoveAt(0);
+                        startIndex++;
+                    }
+                }
+            }
+            
+            return indexCount;
+        }
+    }
+}
---
+++ GemateriaTests/FindGemateriaInTextFile.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GemateriaAssignment.FindMatches;
+
+namespace GemateriaTests
+{
+    class FindGemateriaInTextFileTests
+    {
+        FindGemateriaMathces findGemateriaInTextFile;
+        [SetUp]
+        public void Setup()
+        {
+            findGemateriaInTextFile = new FindGemateriaMathces();
+        }
+
+        [Test]
+        public void Get_All_Instances_Of_Consecutive_numbers_That_Are_Equlas_To_N()
+        {
+            List<int> numbers = new List<int>
+            {
+                2,
+                4,
+                50,
+                10,
+                5,
+                5,
+                4,
+                6,
+                80,
+                10,
+                3,
+                3,
+                3,
+                1,
+                10
+            };
+            int sum = 10;
+            Dictionary<int, int> indexCountDict = findGemateriaInTextFile.GetAllSums(numbers, sum);
+
+            Dictionary<int, int> expected = new Dictionary<int, int>()
+            {
+                { 3,1},
+                {4,2 },
+                {6,2 },
+                {9, 1 },
+                {10, 4 },
+                {14, 1 }
+            };
+
+            bool dictsAreEqual = expected.Count == indexCountDict.Count && !expected.Except(indexCountDict).Any();
+
+            Assert.IsTrue(dictsAreEqual);
+        }
+    }
+}
